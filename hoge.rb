299c0c074f2a20b2a0242b20c24ce97f7c26c608
jf712@Reiankyo <jf712@kmc.gr.jp>
2016-01-25 19:39:55 +0900

    計算めんどうだったので自動でsums.txtに

---
+++ hoge.rb
@@ -0,0 +1,30 @@
+# coding: utf-8
+all_damage = []
+all_assist = []
+all_sum = []
+
+# 直近100戦の平均を返す
+def average(arr)
+  return arr.reverse[0..99].inject(:+)/arr.size
+end
+
+open("62A.txt") do |f|
+  f.each_line do |line|
+    data = line.chomp!.split(",")
+    damage, assist = data[0].to_i, data[1].to_i
+    assist = 0 if assist == "-"
+    
+    sum = damage + assist
+
+    all_damage << damage
+    all_assist << assist
+    all_sum << sum
+  end
+end
+
+open("62A_sums.txt", "w") do |f|
+  message = "直近100戦の記録は以下"
+  aves = "#{average(all_damage)},#{average(all_assist)},#{average(all_sum)}"
+  f.write(message + "\n" + aves + "\n\n" + all_sum.join("\n"))
+end
+
